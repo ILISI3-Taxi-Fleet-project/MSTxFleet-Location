b11329436412276cd35a152442b14bea8606163a
yassine <yassine@gmail.com>
2023-11-14 12:23:21 +0100

    #1 The location service now is able to produce LocationMessage into the kafka topic named location.

--- src/main/java/com/ilisi/mstxfleetlocation/MsTxFleetLocationApplication.java
+++ src/main/java/com/ilisi/mstxfleetlocation/MsTxFleetLocationApplication.java
@@ -2,8 +2,10 @@ package com.ilisi.mstxfleetlocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka // for the configuration of Kafka
 public class MsTxFleetLocationApplication {
 
     public static void main(String[] args) {
