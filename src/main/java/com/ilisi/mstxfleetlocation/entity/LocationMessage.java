9eb512b7722f8e0daf6632cbe4aafee7d2c77351
yassine <yassine@gmail.com>
2023-11-23 16:17:59 +0100

    #3 timestamp added.

--- src/main/java/com/ilisi/mstxfleetlocation/entity/LocationMessage.java
+++ src/main/java/com/ilisi/mstxfleetlocation/entity/LocationMessage.java
@@ -1,11 +1,21 @@
 package com.ilisi.mstxfleetlocation.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LocationMessage {
+    private String userId;
     private double latitude;
     private double longitude;
+    private String userType;
+    private String timestamp = new Date().toString();
 }
