9e245fc5b43ab6389a870a4de1797c33dd535460
musdev <w0l1d.dev@gmail.com>
2023-12-10 18:03:11 +0100

    #8 change locationMessage to Map of userLocation

--- src/main/java/com/ilisi/mstxfleetlocation/controller/LocationController.java
+++ src/main/java/com/ilisi/mstxfleetlocation/controller/LocationController.java
@@ -9,19 +9,31 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.util.Map;
+import java.util.Objects;
+
 @Controller
 @Slf4j
 @AllArgsConstructor
 public class LocationController {
 
-    private final KafkaTemplate<String, LocationMessage> kafkaTemplate;
+    private final KafkaTemplate<String, Map<String, ?>> kafkaTemplate;
 
     @MessageMapping("/location")
     public void getLocation(@Payload LocationMessage message, SimpMessageHeaderAccessor headerAccessor) {
-        String userId = headerAccessor.getSessionAttributes().get("userId").toString();
+        String userId = Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("userId").toString();
         message.setUserId(userId);
-        kafkaTemplate.send("location", message);
-        log.info("Message sent to Kafka topic location: {}", message);
+
+        Map<String, Object> data = Map.of(
+                "userId", message.getUserId(),
+                "location", message.getLocation(),
+                "userType", message.getUserType(),
+                "createdAt", message.getCreatedAt().toString(),
+                "updatedAt", Objects.toString(message.getUpdatedAt(), ""),
+                "isOnline", message.getIsOnline()
+        );
+        kafkaTemplate.send("location", data);
+        log.info("Message sent to Kafka topic location: {}", data);
     }
 
 }
